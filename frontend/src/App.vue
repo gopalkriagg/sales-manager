1ab2cceef221316fc8ebaa6e1e4673f0fdd5b62f
Gopal Krishan Aggarwal <gopalkriagg@gmail.com>
2021-01-03 13:34:02 +0530

    Show clients

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <!--  -->
-    </v-navigation-drawer>
+    <v-app>
+        <v-navigation-drawer v-model="drawer" app>
+            <!--  -->
+        </v-navigation-drawer>
 
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-app-bar app>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Frozen Products - Sales Manager</v-toolbar-title>
-    </v-app-bar>
+            <v-toolbar-title>Frozen Products - Sales Manager</v-toolbar-title>
+        </v-app-bar>
 
-    <v-main>
-      <!--  -->
-    </v-main>
-  </v-app>
+        <v-main>
+            <router-view />
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-  export default {
-    data: () => ({ drawer: null }),
-  }
+export default {
+    data: () => ({ drawer: null })
+};
 </script>
