8299366fa058da83778ef7bcec70078d9d3d394d
Gopal Krishan Aggarwal <gopalkriagg@gmail.com>
2021-01-03 13:41:08 +0530

    Show add client button and search button

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,38 +1,60 @@
 <template>
-    <v-data-table
-        :headers="headers"
-        :items="clients"
-        :items-per-page="5"
-        class="elevation-1"
-    />
+    <v-card width="800">
+        <v-card-title>
+            <v-row>
+                <v-col cols="2">
+                    <v-btn color="primary">
+                        Add Client
+                    </v-btn>
+                </v-col>
+                <v-col cols="10">
+                    <v-text-field
+                        v-model="search"
+                        append-icon="mdi-magnify"
+                        label="Search"
+                        single-line
+                        hide-details
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+        </v-card-title>
+        <v-data-table
+            :headers="headers"
+            :items="clients"
+            :items-per-page="5"
+            :search="search"
+            class="elevation-1"
+        />
+    </v-card>
 </template>
 
 <script>
 export default {
     data() {
         return {
+            search: "",
             headers: [
                 {
                     text: "Client",
                     align: "start",
                     value: "name"
-                },
+                }
             ],
             clients: [
                 {
-                    name: "Ludhiana Client",
+                    name: "Ludhiana Client"
                 },
                 {
-                    name: "Lollipop",
+                    name: "Lollipop"
                 },
                 {
-                    name: "Honeycomb",
+                    name: "Honeycomb"
                 },
                 {
-                    name: "Donut",
+                    name: "Donut"
                 },
                 {
-                    name: "KitKat",
+                    name: "KitKat"
                 }
             ]
         };
