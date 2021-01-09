cefb178126469e5fdea726ac5a1ba96dbed0730e
Gopal Krishan Aggarwal <gopalkriagg@gmail.com>
2021-01-09 13:28:42 +0530

    Show nav drawer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,44 @@
 <template>
-    <v-app>
-        <v-navigation-drawer v-model="drawer" app>
-            <!--  -->
-        </v-navigation-drawer>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list expand shaped nav>
+        <v-list-item color="primary" :exact="true" :to="{ name: 'Home' }">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              Clients
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item color="primary" :exact="true" :to="{ name: 'Product' }">
+          <v-list-item-action>
+            <v-icon>mdi-package-variant</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              Clients
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-app-bar app>
-            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-            <v-toolbar-title>Frozen Products - Sales Manager</v-toolbar-title>
-        </v-app-bar>
+      <v-toolbar-title>Frozen Products - Sales Manager</v-toolbar-title>
+    </v-app-bar>
 
-        <v-main>
-            <router-view />
-        </v-main>
-    </v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
-    data: () => ({ drawer: null })
+  data: () => ({ drawer: null }),
 };
 </script>
